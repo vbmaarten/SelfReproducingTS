051ee89b55a6494452aaa3791d5fb22182d35568
Maarten van Beek <maarten.van.beek@nn.nl>
2022-05-04 16:27:23 +0200

    More solutions

---
+++ self_reproducing3.ts
@@ -0,0 +1,1 @@
+[".forEach(v => console.log('['+String.fromCharCode(34)+v+String.fromCharCode(34)+']'+v))"].forEach(v => console.log('['+String.fromCharCode(34)+v+String.fromCharCode(34)+']'+v))
---
+++ self_reproducing4.ts
@@ -0,0 +1,1 @@
+((x: string) => console.log(x+String.fromCharCode(34)+x+String.fromCharCode(34)+')'))("((x: string) => console.log(x+String.fromCharCode(34)+x+String.fromCharCode(34)+')'))(")
