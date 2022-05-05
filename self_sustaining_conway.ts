bd6f633a874c5d9742cbc53ce5f5005609111c11
Maarten van Beek <maarten.van.beek@nn.nl>
2022-05-05 14:58:06 +0200

    Finished self sustaining conway script

--- self_sustaining_conway.ts
+++ self_sustaining_conway.ts
@@ -1,22 +1,22 @@
 const conway = [
-    [0, 1, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0, 0],
-    [1, 1, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0]
+  [ 0, 1, 0, 0, 0, 0 ],
+  [ 0, 0, 1, 0, 0, 0 ],
+  [ 1, 1, 1, 0, 0, 0 ],
+  [ 0, 0, 0, 0, 0, 0 ],
+  [ 0, 0, 0, 0, 0, 0 ],
+  [ 0, 0, 0, 0, 0, 0 ]
 ] .map((row, rowI, array) => row.map((cell, colI) => {
     const neighbours = array.filter((_, rowIndex) => Math.abs(rowI-rowIndex)<=1)
                             .map(nRow => nRow.filter((_, colIndex) => Math.abs(colI-colIndex)<=1))
-                            .flat()
+                            .reduce((prev, cur) => [...prev, ...cur], [])
                             .reduce((p,a)=>p+a,0)-cell
     
     return cell === 1 ? ((neighbours === 2 || neighbours === 3) ? 1 : 0): (neighbours === 3 ? 1 : 0)
-})); ((s, l) => console.log("const conway =", conway, s,l,"))("+String.fromCharCode(96)+s+String.fromCharCode(96)))(`.map((row, rowI, array) => row.map((cell, colI) => {
+})); ((s, l) => console.log("const conway =", conway, s,l+s+"`, "+String.fromCharCode(39)+l+String.fromCharCode(39)+");"))(`.map((row, rowI, array) => row.map((cell, colI) => {
     const neighbours = array.filter((_, rowIndex) => Math.abs(rowI-rowIndex)<=1)
                             .map(nRow => nRow.filter((_, colIndex) => Math.abs(colI-colIndex)<=1))
-                            .flat()
+                            .reduce((prev, cur) => [...prev, ...cur], [])
                             .reduce((p,a)=>p+a,0)-cell
     
     return cell === 1 ? ((neighbours === 2 || neighbours === 3) ? 1 : 0): (neighbours === 3 ? 1 : 0)
-}));`, '((s, l) => console.log("const conway =", conway, s,l,"))"');
+}));`, '((s, l) => console.log("const conway =", conway, s,l+s+"`, "+String.fromCharCode(39)+l+String.fromCharCode(39)+");"))(`');
